0bb394e6bafb8bf021ab4504976891e7e17586a6
McMul <McMul@LAPTOP-91GTAKB2>
2025-12-22 17:13:58 -0500

    version bump

--- SysBot.Pokemon/Helpers/DudeBot.cs
+++ SysBot.Pokemon/Helpers/DudeBot.cs
@@ -2,7 +2,7 @@ namespace SysBot.Pokemon.Helpers
 {
     public static class DudeBot
     {
-        public const string Version = "v1.3.2";
+        public const string Version = "v1.3.3";
         public const string Attribution = "https://Chinchou.net";
         public const string ConfigPath = "config.json";
     }
